a3f81c5c9249c18ae7b6549eff48bd6f77095119
ych3r <liuyc26@gmail.com>
2023-01-13 14:21:42 -0500

    update homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CS5610 - Web Development.
+          Welcome to CS5610 - Web Development.
         </p>
         <a
           className="App-link"
