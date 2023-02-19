ff5cab755b48d3f81cf8b644aff93b88924b297b
ych3r <liuyc26@gmail.com>
2023-02-19 15:07:24 -0500

    challenge

--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -4,11 +4,11 @@ const NavigationSidebar = () => {
             <a class="list-group-item" href="/">
                 <i class="fab fa-twitter"></i>
             </a>
-            <a class="list-group-item list-group-item-action" href="/">
+            <a class="list-group-item list-group-item-action" href="../HomeScreen/index.html">
                 <i class="fa-solid fa-house-chimney"></i>
                 <span>Home</span>
             </a>
-            <a class="list-group-item list-group-item-action" href="/">
+            <a class="list-group-item list-group-item-action" href="../explore/index.html">
                 <i class="fa-solid fa-hashtag"></i>
                 <span>Explore</span>
             </a>
