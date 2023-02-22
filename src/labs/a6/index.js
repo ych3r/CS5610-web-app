3aa9f6be2ede98f5a824458f0a1047f8a2eba957
ych3r <liuyc26@gmail.com>
2023-02-22 12:33:20 -0500

    finish 2.11

---
+++ src/labs/a6/index.js
@@ -0,0 +1,14 @@
+import Classes from "./classes";
+import Styles from "./styles";
+
+function Assignment6() {
+  return (
+    <div>
+      <h1>Assignment 6</h1>
+      <Styles />
+      <Classes />
+    </div>
+  );
+}
+
+export default Assignment6;
