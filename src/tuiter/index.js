c52b0e7ddd2174df18bdb6d81126a91c81b977cc
ych3r <liuyc26@gmail.com>
2023-02-23 11:21:45 -0500

    tuiter

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,10 +1,22 @@
-import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowList from "./who-to-follow-list";
+import ExploreComponent from "./explore";
 
 function Tuiter() {
   return (
-    <div>
-      <Nav />
-      <h1>Tuiter</h1>
+    <div className="row mt-2">
+      <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+        <NavigationSidebar active="explore" />
+      </div>
+      <div
+        className="col-10 col-md-10 col-lg-7 col-xl-6"
+        style={{ position: "relative" }}
+      >
+        <ExploreComponent />
+      </div>
+      <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+        <WhoToFollowList />
+      </div>
     </div>
   );
 }
