d5f3a0b912c2bb20f8462bf4e382b01936ff7868
ych3r <liuyc26@gmail.com>
2023-04-02 11:59:39 -0400

    a8

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -3,7 +3,6 @@ import axios from "axios";
 // const TUITS_API = "http://localhost:4000/api/tuits";
 // const TUITS_API = "https://cs5610-node-server-app.onrender.com/api/tuits";
 const API_BASE = process.env.REACT_APP_API_BASE;
-console.log(process.env.REACT_APP_API_BASE);
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
